f3542ad59b0a76e5077eba88f4ddc43042c269cd
Matthew Edmond <medmond95@gmail.com>
2016-07-22 13:30:34 -0400

    removed configure

--- app.js
+++ app.js
@@ -1,36 +1,25 @@
-/* var express = require('express');
+var express = require('express');
 var app = express();
 var request = require("request");
 
 var port = process.env.PORT || 3000;
 
+
+
+app.use(bodyParser.urlencoded({ extended: false } ));
+
+
 // Create a route to respond to a webhook POST request
 app.post('/listen', function(req, res) {
     console.log('Hey we got a POST request');
 	console.log(req.body);
-	
+	res.send({ status: 'SUCCESS' });
 
 });
 
-
-
-
- */
-var express = require('express'),
-    app     = express(),
-   	request = require('request');
-	
-app.configure(function(){
-  app.use(express.bodyParser());
-  app.use(app.router);
-});
-
-app.listen(port);
-
-app.post("/listen", function(req, res) {
-  console.log(req.body);
-  res.send({ status: 'SUCCESS' });
-});
 
 
 app.listen(process.env.PORT || 3000);
+
+
+
